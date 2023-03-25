745f5f3cb45d731fbd0faf123796d030b4b30f85
Danh Pham <danhpham612@gmail.com>
2023-03-24 23:40:22 -0700

    3rd commit

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,10 +1,38 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import './App.css';
+import './index.css';
+
+// function App() {
+//   return (
+//     <div className="h-screen bg-white>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 function Hello() {
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
   return (
-    <div>
+    <div className="bg-white dark:bg-black">
       <h1>Hi Mom</h1>
+      <button type="button" onClick={handleThemeSwitch}>
+        Dark Mode
+      </button>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx,ejs}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
