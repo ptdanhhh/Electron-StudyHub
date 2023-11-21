d8e2b14b0d2e03c58601886731999e077271c340
Danh Pham <danhpham612@gmail.com>
2023-11-20 18:31:43 -0800

    update api key

--- client/src/renderer/components/ChatAI.jsx
+++ client/src/renderer/components/ChatAI.jsx
@@ -10,10 +10,9 @@ import {
 } from '@chatscope/chat-ui-kit-react';
 import Sidebar from './Sidebar/Sidebar';
 
-const API_KEY = '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';
-// "Explain things like you would to a 10 year old learning how to code."
+const API_KEY = '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';
+
 const systemMessage = {
-  //  Explain things like you're talking to a software professional with 5 years of experience.
   role: 'system',
   content: "Explain things like you're talking to a university freshmen.",
 };
@@ -39,18 +38,11 @@ function ChatAI() {
 
     setMessages(newMessages);
 
-    // Initial system message to determine ChatGPT functionality
-    // How it responds, how it talks, etc.
     setIsTyping(true);
     await processMessageToChatGPT(newMessages);
   };
 
   async function processMessageToChatGPT(chatMessages) {
-    // messages is an array of messages
-    // Format messages for chatGPT API
-    // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
-    // So we need to reformat
-
     let apiMessages = chatMessages.map((messageObject) => {
       let role = '';
       if (messageObject.sender === 'ChatGPT') {
@@ -61,15 +53,9 @@ function ChatAI() {
       return { role: role, content: messageObject.message };
     });
 
-    // Get the request body set up with the model we plan to use
-    // and the messages which we formatted above. We add a system message in the front to'
-    // determine how we want chatGPT to act.
     const apiRequestBody = {
       model: 'gpt-3.5-turbo',
-      messages: [
-        systemMessage, // The system message DEFINES the logic of our chatGPT
-        ...apiMessages, // The messages from our chat with ChatGPT
-      ],
+      messages: [systemMessage, ...apiMessages],
     };
 
     await fetch('https://api.openai.com/v1/chat/completions', {
@@ -100,24 +86,20 @@ function ChatAI() {
     <div className="flex flex-row">
       <Sidebar />
       <div className="basis-full">
-        <MainContainer>
-          <ChatContainer>
-            <MessageList
-              scrollBehavior="smooth"
-              typingIndicator={
-                isTyping ? (
-                  <TypingIndicator content="ChatGPT is typing" />
-                ) : null
-              }
-            >
-              {messages.map((message, i) => {
-                console.log(message);
-                return <Message key={i} model={message} />;
-              })}
-            </MessageList>
-            <MessageInput placeholder="Type message here" onSend={handleSend} />
-          </ChatContainer>
-        </MainContainer>
+        <ChatContainer>
+          <MessageList
+            scrollBehavior="smooth"
+            typingIndicator={
+              isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null
+            }
+          >
+            {messages.map((message, i) => {
+              console.log(message);
+              return <Message key={i} model={message} />;
+            })}
+          </MessageList>
+          <MessageInput placeholder="Type message here" onSend={handleSend} />
+        </ChatContainer>
       </div>
     </div>
   );
